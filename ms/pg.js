4e60e30498d33edff1cce8fd1a1c69eec12b455a
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-06-19 11:52:02 +0100

    change bar colour to background

--- ms/pg.js
+++ ms/pg.js
@@ -19,13 +19,12 @@ var hooks = document.cdnMultiStep.hooks;
   );
 document.getElementById( document.cdnParameters.progress_bar ).innerHTML = '<div class="progress-bar" style="height: 100%;float: left;width: 85%;">' +
                                                                                '<div class="bar-container" style="display: flex;height: 100%;width: 100%;justify-content: center;align-items: center;">' +
-                                                                                  '<div class="bar" id="pgBar" style=" width: 80%;height: 78%;">' +
+                                                                                  '<div class="bar" id="pgBar" style="width: 80%;height: 78%;background-color: #FFA500;">' +
                                                                                     '<div class="progress" id="progress" style="height: 100%;background-color: #FF0000;width: 0%;"></div>' +
                                                                                   '</div>' +
                                                                                '</div>' + 
                                                                            '</div>' + 
                                                                            '<div class="progress-bar-percent" id="percent_value" style="width: 15%;height: 100%;background-color: #ffff00;float: left;"></div>';
-document.getElementById('pgBar').style.background = 'linear-gradient( to right, ' + document.cdnParameters.progress_bar_color1 + ' 0%, ' + document.cdnParameters.progress_bar_color2 + ' 0% 100%';
 document.getElementById('perVal').innerHTML = '0%';
 var setProgress = function( nextPrev ) {
     var step = $( '.active' ).attr( 'data-id' );
