556beaa7417ee3a7a8f0e6f1f76b4946d03bc65b
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-06-19 16:26:49 +0100

    working on page 2

--- taylor/hooks.js
+++ taylor/hooks.js
@@ -10,18 +10,18 @@ var hooks = document.cdnMultiStep.hooks;
 var selectedLenders = [];
 var lenderTypeWrapper = '';
 var lenderTypeTpl =
-  '<div class="selected-lender" >' +
-  '<div class="lender_name">{1}</div>' +
-  '<div class="lender_type">' +
-  '<input type="radio" name="lender_type-{0}" id="loan-{0}" value="Loan" checked/><label for="loan-{0}">Loan</label>' +
-  '</div>' +
-  '<div class="lender_type">' +
-  '<input type="radio" name="lender_type-{0}" id="cc_card-{0}" value="Credit Card" /><label for="cc_card-{0}">Credit Card</label>' +
-  '</div>' +
-  '<div class="lender_type">' +
-  '<input type="radio" name="lender_type-{0}" id="mortgage-{0}" value="Mortgage" /><label for="mortgage-{0}">Mortgage</label>' +
-  '</div>' +
-  '</div>';
+    '<div class="selected-lender" >' +
+    '<div class="lender_name">{1}</div>' +
+    '<div class="lender_type">' +
+    '<input type="radio" name="lender_type-{0}" id="loan-{0}" value="Loan" checked/><label for="loan-{0}">Loan</label>' +
+    '</div>' +
+    '<div class="lender_type">' +
+    '<input type="radio" name="lender_type-{0}" id="cc_card-{0}" value="Credit Card" /><label for="cc_card-{0}">Credit Card</label>' +
+    '</div>' +
+    '<div class="lender_type">' +
+    '<input type="radio" name="lender_type-{0}" id="mortgage-{0}" value="Mortgage" /><label for="mortgage-{0}">Mortgage</label>' +
+    '</div>' +
+    '</div>';
 var customBanks = 0;
 var lenders = [];
 var customBanksArr = [];
@@ -30,87 +30,74 @@ var maxCustomBanks = 3;
 
 //--------------- FUNCTIONS
 function autocomplete() {
-var selector = '#other_1';
-$(selector).attr('multiple', true);
-$(selector).selectize({
-  plugins: ['remove_button'],
-  dropdownParent: 'body',
-  maxItems: maxCustomBanks,
-  openOnFocus: false,
-  hideSelected: true,
-  closeAfterSelect: true,
-  onItemAdd: function(value, $item) {
-    //customBanks++;
-    customBanksArr.push(value);
+    var selector = '#other_1';
+    $(selector).attr('multiple', true);
+    $(selector).selectize({
+        plugins: ['remove_button'],
+        dropdownParent: 'body',
+        maxItems: maxCustomBanks,
+        openOnFocus: false,
+        hideSelected: true,
+        closeAfterSelect: true,
+        onItemAdd: function(value, $item) {
+            //customBanks++;
+            customBanksArr.push(value);
+            $('body').addClass('hide-dropdown');
+            console.log(customBanksArr);
+        },
+        onItemRemove: function(value) {
+            //customBanks--;
+            customBanksArr.removeByValue(value);
+            $('body').addClass('hide-dropdown');
+            console.log(customBanksArr);
+        },
+        onType: function(str) {
+            if (str && str.trim().length) {
+                $('body').removeClass('hide-dropdown');
+            }
+        }
+    });
     $('body').addClass('hide-dropdown');
-    console.log(customBanksArr);
-  },
-  onItemRemove: function(value) {
-    //customBanks--;
-    customBanksArr.removeByValue(value);
-    $('body').addClass('hide-dropdown');
-    console.log(customBanksArr);
-  },
-  onType: function(str) {
-    if (str && str.trim().length) {
-      $('body').removeClass('hide-dropdown');
-    }
-  }
-});
-$('body').addClass('hide-dropdown');
 }
 
 
 var fillLenderFields = function(page) {
-var selected = [];
-lenderTypeWrapper = '';
-isOK = true;
+    var allLender = lenders.push(customBanksArr);
+    lenderTypeWrapper = '';
+    isOK = true;
 
-var allLenders = $(':input:checked, select', '#step-1');
-var checkedID =  0;
+    var allLenders = $(':input:checked, select', '#step-1');
+    var checkedID = 0;
 
-for (var j = 0; j < allLenders.length; j++) {
-  var $l = $(allLenders[j]);
-  if (/*$l.is(':visible') &&*/ $l.val() != 'Select Lender' && $l.val() != 'Other') {
-    lenderTypeWrapper += lenderTypeTpl.format(j, $l.val());
-    selected.push($l.val());
-  } else if ($l.val() == 'Other') {
-    fillCustomBanks(j, selected);
-  }
-}
+    for (var j = 0; j < allLenders.length; j++) {
+        var $l = $(allLenders[j]);
+        lenderTypeWrapper += lenderTypeTpl.format(j, $l.val());
+        selected.push($l.val());
+    }
+    /*
+    if (isArrayEqual(selectedLenders, selected)) {
+        return true;
+    }
 
-// TODO : Do custom errors..
+    $('#lender_types').remove();
 
-/*if (!selected.length || selected.length > 8) {
-  isOK = false;
-  $('#lp-pom-text-1048').show();
-} else {
-  $('#lp-pom-text-1048').hide();
-}*/
+    var fields = $('[type="hidden"][id^="acctype"]');
+    for (var i = 0; i < fields.length; i++) {
+        $(fields[i]).val('');
+    }
+    var lenders = $('[type="hidden"][id^="lender"]');
+    for (var i = 0; i < lenders.length; i++) {
+        $(lenders[i]).val('');
+    }
+    for (var n = 0; n < selected.length; n++) {
+        var id = n + 1;
+        $('#lender_' + id).val(selected[n]);
+    }
 
-if (isArrayEqual(selectedLenders, selected)) {
-  return true;
-}
-
-$('#lender_types').remove();
-
-var fields = $('[type="hidden"][id^="acctype"]');
-for (var i = 0; i < fields.length; i++) {
-  $(fields[i]).val('');
-}
-var lenders = $('[type="hidden"][id^="lender"]');
-for (var i = 0; i < lenders.length; i++) {
-  $(lenders[i]).val('');
-}
-for (var n = 0; n < selected.length; n++) {
-  var id = n + 1;
-  $('#lender_' + id).val(selected[n]);
-}
-
-$('#step-' + page).append('<div id="lender_types">' + lenderTypeWrapper + '</div>');
-selectedLenders = selected;
+    $('#step-' + page).append('<div id="lender_types">' + lenderTypeWrapper + '</div>');
+    selectedLenders = selected;*/
 };
-
+/*
 var fillCustomBanks = function(n, selected) {
 for (var k = 0; k < customBanksArr.length; k++) {
   $('[type="hidden"][id="lender_' + (n+1) + '"]').val(customBanksArr[k]);
@@ -118,8 +105,9 @@ for (var k = 0; k < customBanksArr.length; k++) {
   lenderTypeWrapper += lenderTypeTpl.format((n+1), customBanksArr[k]);
   n++;
 }
-};
+};*/
 
+/*
 var fillLenderTypes = function() {
 var checked = $('[name^=lender_type-]:checked');
 var all = $('[name^=lender_type-]');
@@ -142,20 +130,20 @@ if (checked.length != $selectedLender.length) {
   }
   isOK = true;
 }
-};
+};*/
 
 // Utilities
 var isArrayEqual = function(arr1, arr2) {
-return arr1.toString() === arr2.toString()
+    return arr1.toString() === arr2.toString()
 };
 
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined' ? args[number] : match;
-    });
-  };
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
 }
 
 //--------------- FUNCTIONS
@@ -163,60 +151,65 @@ if (!String.prototype.format) {
 //--------------- EVENTS/HOOKS
 //Hooks Page Init
 hooks.register(
-'hookPageInit',
-function(args) {
-    $('#lender_other').load(autocomplete());
+    'hookPageInit',
+    function(args) {
+        $('#lender_other').load(autocomplete());
 
-    $('input[name=lender]').change(function() {
-      lenders = [];
-      var $checked = $('input[name=lender]:checked');
-      var other = false;
-      for ( i = 0; i < $checked.length; i += 1) {
-          if ($checked[i].value === "Other" ) {
-              other = true;
-              $('#container_other_1').show();
-          } else { lenders.push($checked[i].value); }
-      }
-      if (!other) { $('#container_other_1').hide(); }
-  });
-  return true;
-});
+        $('input[name=lender]').change(function() {
+            lenders = [];
+            var $checked = $('input[name=lender]:checked');
+            var other = false;
+            for (i = 0; i < $checked.length; i += 1) {
+                if ($checked[i].value === "Other") {
+                    other = true;
+                    $('#container_other_1').show();
+                } else {
+                    lenders.push($checked[i].value);
+                }
+            }
+            if (!other) {
+                $('#container_other_1').hide();
+            }
+        });
+        return true;
+    });
 
 //Hooks New Step
 hooks.register(
-'hookNewStep',
-function(args) {
-  if ($('body[data-current-page="1"]').length) {
-    if ($('#lender_other:checked').val() === "Other") {
-    	$('#container_other_1, #container_other_2').show();
-    } else { $('#container_other_1, #container_other_2').hide();  }
-  } else if ($('body[data-current-page="2"]').length) {
-    $('#container_other_1, #container_other_2').toggle();
-    fillLenderFields(2);
-  } else if ($('body[data-current-page="3"]').length) {
+    'hookNewStep',
+    function(args) {
+        if ($('body[data-current-page="1"]').length) {
+            if ($('#lender_other:checked').val() === "Other") {
+                $('#container_other_1').show();
+            } else {
+                $('#container_other_1').hide();
+            }
+        } else if ($('body[data-current-page="2"]').length) {
+            fillLenderFields(2);
+        } else if ($('body[data-current-page="3"]').length) {
 
-  }
-  return true;
-}
+        }
+        return true;
+    }
 );
 
 //Hooks Next Check
 hooks.register(
-'hookNextCheck',
-function(args) {
-  var selected = $(':input:checked, select', '#step-1');
-  if (selected.length === 1) {
-    if (selected.val() === "Other" && customBanksArr.length === 0) {
-      document.getElementById('lender_abbey_national').setCustomValidity('Please choose your other bank/lender below');
-      return false;
-    }
+    'hookNextCheck',
+    function(args) {
+        /*var selected = $(':input:checked, select', '#step-1');
+        if (selected.length === 1) {
+          if (selected.val() === "Other" && customBanksArr.length === 0) {
+            document.getElementById('lender_abbey_national').setCustomValidity('Please choose your other bank/lender below');
+            return false;
+          }
 
-  } else if (selected.length > 8) {
-    document.getElementById('lender_abbey_national').setCustomValidity('Please select 8 or less banks/lenders');
-    return false;
-  }
-  return true;
-}
+        } else if (selected.length > 8) {
+          document.getElementById('lender_abbey_national').setCustomValidity('Please select 8 or less banks/lenders');
+          return false;
+        }
+        return true;*/
+    }
 );
 
 //--------------- EVENTS/HOOKS
