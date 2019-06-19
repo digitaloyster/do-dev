603bad09835697a7b6f0b5e76013f671871a4eea
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-06-19 10:19:56 +0100

    minus 2 from step for prev function

--- ms/pg.js
+++ ms/pg.js
@@ -4,7 +4,7 @@
     'hookPrevCheck',
     function (args) {
       console.log('prev');
-      setProgress();
+      setProgress('prev');
       return true;
     }   
   );
@@ -24,10 +24,13 @@ document.getElementById( document.cdnParameters.progress_bar ).innerHTML = '<div
                                                                            '<div class="progress-bar-percent" id="perVal"></div>';
 document.getElementById('pgBar').style.background = 'linear-gradient( to right, ' + document.cdnParameters.progress_bar_color1 + ' 0%, ' + document.cdnParameters.progress_bar_color2 + ' 0% 100%';
 
-var setProgress = function() {
-    var progress = ( ( $( '.active' ).attr( 'data-id' ) ) / Object.keys( document.cdnMultiStep.steps ).length);
-    progress = Math.abs(progress);
-    $( '#' + document.cdnMultiStep.progressBar + ' > div').data('progresspercent', progress * 100);
+var setProgress = function( nextPrev ) {
+    var step = $( '.active' ).attr( 'data-id' );
+    nextPrev == 'prev' ? step -=2 : step === step;
+    var progress = ( ( step ) / Object.keys( document.cdnMultiStep.steps ).length);
+    pg_per = Math.abs(progress) * 100;
+    document.getElementById('perVal').innerHTML = pg_per;
+    document.getElementById('pgBar').style.background = 'linear-gradient( to right, ' + document.cdnParameters.progress_bar_color1 + ' ' + pg_per +  '%, ' + document.cdnParameters.progress_bar_color2 + ' ' + pg_per + '% 100%';
     var getProgressWrapWidth = $('.progress-wrap').width();
     var progressTotal = progress * getProgressWrapWidth;
     var animationLength = 500;
