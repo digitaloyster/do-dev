eeaabc6576e1bb9bf9e02b98f72035a533427332
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-06-19 16:00:40 +0100

    changed to jquery to get progress bar

--- ms/pg.js
+++ ms/pg.js
@@ -15,14 +15,14 @@ var hooks = document.cdnMultiStep.hooks;
       return true;
     }
   );
-document.getElementById( document.cdnParameters.progress_bar ).innerHTML = '<div class="progress-bar" style="height: 100%;float: left;width: 85%;">' +
-                                                                               '<div class="bar-container" style="display: flex;height: 100%;width: 100%;justify-content: center;align-items: center;">' +
-                                                                                  '<div class="progress_bar" id="pgBar" style="width: 80%;height: 78%;background-color: #FFA500;">' +
-                                                                                    '<div class="progress" id="progress" style="height: 100%;background-color: #FF0000;width: 0%;"></div>' +
-                                                                                  '</div>' +
-                                                                               '</div>' + 
-                                                                            '</div>' + 
-                                                                           '<div class="progress-bar-percent" id="percent_value" style="width: 15%;height: 100%;float: left; display: flex;justify-content: center;align-items: center;"></div>';
+$( document.cdnParameters.progress_bar ).html( '<div class="progress-bar" style="height: 100%;float: left;width: 85%;">' +
+                                                   '<div class="bar-container" style="display: flex;height: 100%;width: 100%;justify-content: center;align-items: center;">' +
+                                                      '<div class="progress_bar" id="pgBar" style="width: 80%;height: 78%;background-color: #FFA500;">' +
+                                                        '<div class="progress" id="progress" style="height: 100%;background-color: #FF0000;width: 0%;"></div>' +
+                                                      '</div>' +
+                                                   '</div>' + 
+                                                '</div>' + 
+                                               '<div class="progress-bar-percent" id="percent_value" style="width: 15%;height: 100%;float: left; display: flex;justify-content: center;align-items: center;"></div>');
 document.getElementById('percent_value').innerHTML = '0%';
 var setProgress = function( nextPrev ) {
     var step = $( '.active' ).attr( 'data-id' );
