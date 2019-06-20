52314703ea5e29d6319662de33b4373b99ec46e4
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-06-20 11:16:26 +0100

    Linted hooks.js

--- taylor/hooks.js
+++ taylor/hooks.js
@@ -4,33 +4,45 @@ multilender hidden csv
 multitype hidden csv
 --------------------------------------------------*/
 
+/*jslint browser: true*/
+/*global $, jQuery, console, alert*/
+
+
 //--------------- VARIABLES
 var hooks = document.cdnMultiStep.hooks;
 
 var selectedLenders = [];
 var lenderTypeWrapper = '';
-var lenderTypeTpl =
-    '<div class="selected-lender" >' +
-    '<div class="lender_name">{1}</div>' +
-    '<div class="lender_type">' +
-    '<input type="radio" name="lender_type-{0}" id="loan-{0}" value="Loan" checked/><label for="loan-{0}">Loan</label>' +
-    '</div>' +
-    '<div class="lender_type">' +
-    '<input type="radio" name="lender_type-{0}" id="cc_card-{0}" value="Credit Card" /><label for="cc_card-{0}">Credit Card</label>' +
-    '</div>' +
-    '<div class="lender_type">' +
-    '<input type="radio" name="lender_type-{0}" id="mortgage-{0}" value="Mortgage" /><label for="mortgage-{0}">Mortgage</label>' +
-    '</div>' +
-    '</div>';
+var lenderTypeTpl = '<div class="selected-lender" >' +
+        '<div class="lender_name">{1}</div>' +
+            '<div class="lender_type"><input type="radio" name="lender_type-{0}" id="loan-{0}" value="Loan" checked/><label for="loan-{0}">Loan</label></div>' +
+                '<div class="lender_type"><input type="radio" name="lender_type-{0}" id="cc_card-{0}" value="Credit Card" /><label for="cc_card-{0}">Credit Card</label></div>' +
+                    '<div class="lender_type"><input type="radio" name="lender_type-{0}" id="mortgage-{0}" value="Mortgage" /><label for="mortgage-{0}">Mortgage</label></div>' +
+                        '</div>';
 var customBanks = 0;
 var lenders = [];
 var customBanksArr = [];
 var maxCustomBanks = 3;
+
+var $submit = $('#' + document.cdnMultiStep.settings.submitButton);
+var submit_text = $submit.find('span').html();
+
 //--------------- VARIABLES
 
 //--------------- FUNCTIONS
+var setCookie = function (ref) {
+    "use strict";
+    var d = new Date(),
+        expires = "expires=" + d.toUTCString(),
+        timeout = "timeout=" + Math.round((new Date()).getTime() / 1000);
+    d.setTime(d.getTime() + (30 * 60 * 1000));
+    document.cookie = "reference=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "reference=" + ref + ";" + expires + ";path=/";
+    document.cookie = timeout + ";" + expires + ";path=/";
+};
 
-function autocomplete() {
+var autocomplete = function () {
+    "use strict";
     var selector = '#other_1';
     $(selector).attr('multiple', true);
     $(selector).selectize({
@@ -40,35 +52,35 @@ function autocomplete() {
         openOnFocus: false,
         hideSelected: true,
         closeAfterSelect: true,
-        onItemAdd: function(value, $item) {
-            //customBanks++;
+        onItemAdd: function (value) {
             customBanksArr.push(value);
             $('body').addClass('hide-dropdown');
             console.log(customBanksArr);
         },
-        onItemRemove: function(value) {
+        onItemRemove: function (value) {
             //customBanks--;
             customBanksArr.removeByValue(value);
             $('body').addClass('hide-dropdown');
             console.log(customBanksArr);
         },
-        onType: function(str) {
+        onType: function (str) {
             if (str && str.trim().length) {
                 $('body').removeClass('hide-dropdown');
             }
         }
     });
     $('body').addClass('hide-dropdown');
-}
+};
 
 
-var fillLenderFields = function(page) {
-    var all = [];
+var fillLenderFields = function (page) {
+    "use strict";
+    var all = [], j, i, multitypesetting, multitypesettings;
     all.extend(lenders);
     all.extend(customBanksArr);
     lenderTypeWrapper = '';
 
-    for (var j = 0; j < all.length; j++) {
+    for (j = 0; j < all.length; j += 1) {
         lenderTypeWrapper += lenderTypeTpl.format(j, all[j]);
     }
 
@@ -76,43 +88,46 @@ var fillLenderFields = function(page) {
     $('#lender_types').remove();
     $('#step-' + page).append('<div id="lender_types">' + lenderTypeWrapper + '</div>');
 
-    var multitypesetting = $('#multitype')[0].value;
+    multitypesetting = $('#multitype')[0].value;
     if (multitypesetting !== '') {
-        var multitypesettings = multitypesetting.split(',');
-        for ( i = 0; i < multitypesettings.length ; i += 1) {
-            if (multitypesettings[i] === "Credit Card") { $('#cc_card-' + i).attr('checked', 'true'); }
-            else if (multitypesettings[i] === "Mortgage") { $('#mortgage-' + i).attr('checked', 'true'); }
+        multitypesettings = multitypesetting.split(',');
+        for (i = 0; i < multitypesettings.length; i += 1) {
+            if (multitypesettings[i] === "Credit Card") { $('#cc_card-' + i).attr('checked', 'true');
+                } else if (multitypesettings[i] === "Mortgage") { $('#mortgage-' + i).attr('checked', 'true'); }
         }
     }
 };
 
 var fillTypeField = function () {
-    var allTypes = [];
-    var all = [];
+    "use strict";
+    var allTypes = [], all = [], i;
     all.extend(lenders);
     all.extend(customBanksArr);
-    for ( i = 0; i < all.length ; i += 1 ) {
-        allTypes.push( $('input[name=lender_type-' + i + ']:checked')[0].value );
+    for (i = 0; i < all.length; i += 1) {
+        allTypes.push($('input[name=lender_type-' + i + ']:checked')[0].value);
     }
     $('#multitype').val(allTypes.join(','));
 };
 
 // Utilities
-var isArrayEqual = function(arr1, arr2) {
-    return arr1.toString() === arr2.toString()
+var isArrayEqual = function (arr1, arr2) {
+    "use strict";
+    return arr1.toString() === arr2.toString();
 };
 
 if (!Array.prototype.extend) {
     Array.prototype.extend = function (other_array) {
-        other_array.forEach(function(v) {this.push(v)}, this);
+        "use strict";
+        other_array.forEach(function (v) {this.push(v); }, this);
     };
 }
 
 if (!String.prototype.format) {
-    String.prototype.format = function() {
+    String.prototype.format = function () {
+        "use strict";
         var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-            return typeof args[number] != 'undefined' ? args[number] : match;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return args[number] !== 'undefined' ? args[number] : match;
         });
     };
 }
@@ -123,13 +138,13 @@ if (!String.prototype.format) {
 //Hooks Page Init
 hooks.register(
     'hookPageInit',
-    function(args) {
+    function (args) {
+        "use strict";
         $('#lender_other').load(autocomplete());
 
-        $('input[name=lender]').change(function() {
+        $('input[name=lender]').change(function () {
             lenders = [];
-            var $checked = $('input[name=lender]:checked');
-            var other = false;
+            var $checked = $('input[name=lender]:checked'), other = false, i;
             for (i = 0; i < $checked.length; i += 1) {
                 if ($checked[i].value === "Other") {
                     other = true;
@@ -143,12 +158,14 @@ hooks.register(
             }
         });
         return true;
-    });
+    }
+);
 
 //Hooks New Step
 hooks.register(
     'hookNewStep',
-    function(args) {
+    function (args) {
+        "use strict";
         if ($('body[data-current-page="1"]').length) {
             if ($('#lender_other:checked').val() === "Other") {
                 $('#container_other_1').show();
@@ -159,7 +176,7 @@ hooks.register(
             fillLenderFields(2);
         } else if ($('body[data-current-page="3"]').length) {
             fillTypeField();
-            console.log($('#multitype'));
+            //console.log($('#multitype'));
         }
         return true;
     }
@@ -168,19 +185,70 @@ hooks.register(
 //Hooks Next Check
 hooks.register(
     'hookNextCheck',
-    function(args) {
+    function (args) {
+        "use strict";
+        //TODO : Need check for blank other, 0 selects and >8 selects
         /*var selected = $(':input:checked, select', '#step-1');
         if (selected.length === 1) {
-          if (selected.val() === "Other" && customBanksArr.length === 0) {
+            if (selected.val() === "Other" && customBanksArr.length === 0) {
             document.getElementById('lender_abbey_national').setCustomValidity('Please choose your other bank/lender below');
             return false;
-          }
+        }
 
         } else if (selected.length > 8) {
-          document.getElementById('lender_abbey_national').setCustomValidity('Please select 8 or less banks/lenders');
-          return false;
-      }*/
+        document.getElementById('lender_abbey_national').setCustomValidity('Please select 8 or less banks/lenders');
+        return false;
+        }*/
         return true;
+    }
+);
+
+//Hooks Submit
+hooks.register(
+    'hookNextCheck',
+    function (args) {
+        "use strict";
+        $submit.unbind("click");
+        $submit.find('span').html('<strong>Please wait...</strong>');
+        var formData = new FormData(), $fields  = $(':input').not('[type="file"], [type="button"], button');
+        $fields.each(function (i, val) {
+            var name = $(val).attr('name'), value = $(val).val();
+            formData.append(name, value);
+        });
+    //formData.append("lenders", multiLender());
+
+        $.ajax({
+            type: "post",
+            //url: "https://digitaloyster.co.uk/dev/sigform_taylor/API-Stage-1.php",
+            url: "https://digitaloyster.co.uk/dev/sigform_taylor/test_page.php",
+            data: formData,
+            dataType: "text",
+            cache: false,
+            contentType: false,
+            processData: false,
+            crossDomain : true
+        })
+            .done(function (data) {
+                if (data === "email exists") {
+                    alert("Thank you, We have received your application.");
+                    console.log(data);
+                    return false;
+                }
+                console.log("Ajax Done");
+                console.log(data);
+                $('#reference').val(data);
+                setCookie(data);
+                //alert("Submitted");
+                return true;
+            })
+            .fail(function () {
+                var d = new Date(), message = "Fail: " + $('#ckm_request_id').val() + " - " + d.toUTCString();
+                $.get("https://digitaloyster.co.uk/dev/sigform_taylor/error_logging.php", { msg: message});
+                alert("ajax error");
+                $submit.on("click", submitHandle);
+                $submit.find('span').html(submit_text);
+                return false;
+            });
     }
 );
 
