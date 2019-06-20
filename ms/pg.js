fef890290da3c4cd421ccd8758d22ab62cd5b2ee
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-06-20 14:31:02 +0100

    border width change

--- ms/pg.js
+++ ms/pg.js
@@ -19,7 +19,7 @@ var fontSize =  progHeight / 2 ;
 if( fontSize < 8 ){ fontSize = 8; }
 if( fontSize > 18 ){ fontSize = 18; }
 $( document.cdnParameters.progress_bar ).html('<div class="bar-container" style="display: flex;height: 100%;width: 100%;justify-content: center;align-items: center;">' +
-                                                  '<div class="progress_bar_background progress_bar_border_color" id="pgBar" style="width: 96.5%;height: 78%;background-color: #FFA500;border: solid 1px;border-color:#000;border-radius: 8px;">' +
+                                                  '<div class="progress_bar_background progress_bar_border_color" id="pgBar" style="width: 96.5%;height: 78%;background-color: #FFA500;border: solid 2px;border-color:#000;border-radius: 8px;">' +
                                                      '<div class="progress_bar_color progress_bar_font_color" id="progress" style="font-size:' + fontSize + 'px;height: 100%;background-color: #FF0000;width: 0%;display: flex;justify-content: center;align-items: center;border-radius: 8px 0px 0px 8px;"></div>' +
                                                   '</div>' +
                                                '</div>');
