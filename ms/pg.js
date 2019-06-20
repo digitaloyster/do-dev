e21917e257f7182a6a4c376e79297bd5c760ab9b
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-06-20 11:13:17 +0100

    changed the progress bar into flex box

--- ms/pg.js
+++ ms/pg.js
@@ -17,7 +17,7 @@ hooks.register(
 $( document.cdnParameters.progress_bar ).html( '<div class="progress-bar" style="height: 100%;float: left;width: 85%;">' +
 						   '<div class="bar-container" style="display: flex;height: 100%;width: 100%;justify-content: center;align-items: center;">' +
 						      '<div class="progress_bar_background progress_bar_border_color" id="pgBar" style="width: 80%;height: 78%;background-color: #FFA500;border: solid 1px;border-color:#000;border-radius: 8px;">' +
-							'<div class="progress_bar_color" id="progress" style="height: 100%;background-color: #FF0000;width: 0%;"></div>' +
+							'<div class="progress_bar_color" id="progress" style="height: 100%;background-color: #FF0000;width: 0%;display: flex;justify-content: center;align-items: center;"></div>' +
 						      '</div>' +
 						   '</div>' + 
 						'</div>');
