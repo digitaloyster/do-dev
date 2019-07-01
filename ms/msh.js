61166e765b971fa7fa88959106948900553551bb
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-07-01 11:11:58 +0100

    Added logic for loading jqui

--- ms/msh.js
+++ ms/msh.js
@@ -26,19 +26,32 @@ document.cdnMultiStep.hooks = {
 };
 
 
+if (document.cdnMultiStep.steps != '') {
+    var steps = document.cdnMultiStep.steps;
+} else alert('steps not found');
+
+
 /*var script = document.createElement('script');
 script.setAttribute('src', 'https://cdn.jsdelivr.net/gh/fengyuanchen/datepicker@latest/dist/datepicker.min.js');
 script.setAttribute('id', 'date-picker-script');
 document.head.appendChild(script);*/
-if ("display" in val && val.display == "datepicker") {
-    var jquiStyle = document.createElement('link');
-    jquiStyle.setAttribute('href', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css');
-    jquiStyle.setAttribute('rel', 'stylesheet');
-    document.head.appendChild(jquiStyle);
 
-    var jquiScript = document.createElement('script');
-    jquiScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js');
-    document.head.appendChild(jquiScript);
-}
+$.each(steps, function(i, val) {
+    if ("fields" in steps[i] && steps[i].fields != '') {
+        $.each(steps[i].fields, function(k, val) {
+            if ("display" in val && val.display == "datepicker" && !$('#jqui').length) {
+                var jquiStyle = document.createElement('link');
+                jquiStyle.setAttribute('href', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css');
+                jquiStyle.setAttribute('rel', 'stylesheet');
+                document.head.appendChild(jquiStyle);
+
+                var jquiScript = document.createElement('script');
+                jquiScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js');
+                jquiScript.setAttribute('id', 'jqui');
+                document.head.appendChild(jquiScript);
+            }
+        });
+    }
+});
 /*<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>*/
