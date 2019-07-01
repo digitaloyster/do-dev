68c9e6a6d0dadf1c6797fb17124facf5ed2b1956
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-07-01 13:31:46 +0100

    make the loop properly :P

--- ms/msh.js
+++ ms/msh.js
@@ -63,7 +63,7 @@ function loadMSB() {
         msbScript.setAttribute('id', 'msbScript');
         document.body.appendChild(msbScript);
     }
-    else { window.setTimeout( checkJqueryUI, 50 ); }
+    else { window.setTimeout( loadMSB(), 50 ); }
 }
 
 loadMSB();
