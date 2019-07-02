7d42ef97b7efa4ed10d3fc9f0f33bd74c982bcb7
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-07-02 13:29:03 +0100

    trying inputmode = numeric

--- ms/msb-jqui-2.js
+++ ms/msb-jqui-2.js
@@ -100,6 +100,7 @@ $(window).bind("load", function() {
                     if ("numeric" in val && val.numeric == "Y") {
                         var field = document.getElementById(k);
                         //field.type = "number";
+                        field.setAttribute('inputmode','numeric');
                         field.setAttribute('pattern', '[0-9]*');
                     }
                 });
