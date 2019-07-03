2c303d25da6fb155cb265469c81dcac9dc08c998
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-07-03 15:34:23 +0100

    Changed format option

--- ms/msb-pika.js
+++ ms/msb-pika.js
@@ -70,29 +70,29 @@ $(window).bind("load", function() {
                     if ("display" in val && val.display == "datepicker") {
                         var dt = new Date();
                         var yearEnd = dt.getYear() + 1900;
-  	                     var yearStart = yearEnd - 120;
-                         var picker = new Pikaday({
-                             field: document.getElementById(k),
-                             firstDay:1,
-                             format: 'D/M/YYYY',
-                             yearRange: [yearStart, yearEnd],
-                             toString(date, format) {
-                                 // you should do formatting based on the passed format,
-                                 // but we will just return 'D/M/YYYY' for simplicity
-                                 const day = date.getDate();
-                                 const month = date.getMonth() + 1;
-                                 const year = date.getFullYear();
-                                 return `${year}-${month}-${day}`;
-                             },
-                             parse(dateString, format) {
-                                 // dateString is the result of `toString` method
-                                 const parts = dateString.split('/');
-                                 const day = parseInt(parts[2], 10);
-                                 const month = parseInt(parts[1], 10) - 1;
-                                 const year = parseInt(parts[0], 10);
-                                 return new Date(year, month, day);
-                             }
-                         });
+                        var yearStart = yearEnd - 120;
+                        var picker = new Pikaday({
+                            field: document.getElementById(k),
+                            firstDay: 1,
+                            format: 'DD/MM/YYYY',
+                            yearRange: [yearStart, yearEnd],
+                            toString(date, format) {
+                                // you should do formatting based on the passed format,
+                                // but we will just return 'D/M/YYYY' for simplicity
+                                const day = date.getDate();
+                                const month = date.getMonth() + 1;
+                                const year = date.getFullYear();
+                                return `${year}-${month}-${day}`;
+                            },
+                            parse(dateString, format) {
+                                // dateString is the result of `toString` method
+                                const parts = dateString.split('/');
+                                const day = parseInt(parts[2], 10);
+                                const month = parseInt(parts[1], 10) - 1;
+                                const year = parseInt(parts[0], 10);
+                                return new Date(year, month, day);
+                            }
+                        });
                     }
 
 
