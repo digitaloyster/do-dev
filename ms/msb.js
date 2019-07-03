b10e5917f4b7c169abeaa5992892e444fe8a6da6
Timothy Digital Oyster <timothy@digitaloyster.uk>
2019-07-03 15:19:40 +0100

    Linked to the dev CSS

--- ms/msb.js
+++ ms/msb.js
@@ -91,7 +91,8 @@ $(document).ready(function() {
         });
 
         var styles = document.createElement('link');
-        styles.setAttribute('href', 'https://cdn.jsdelivr.net/gh/digitaloyster/do-live/ms/ms.css');
+        //styles.setAttribute('href', 'https://cdn.jsdelivr.net/gh/digitaloyster/do-live/ms/ms.css');
+        styles.setAttribute('href', 'https://digitaloyster.github.io/do-dev/ms/msb.css');
         styles.setAttribute('rel', 'stylesheet');
         styles.setAttribute('type', 'text/css');
         document.head.appendChild(styles);
