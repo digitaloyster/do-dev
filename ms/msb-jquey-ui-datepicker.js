e88f256f474629d7fe2afb344d65a8b1ea76e568
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-07-08 12:08:46 +0100

    update to regex date

--- ms/msb-jquey-ui-datepicker.js
+++ ms/msb-jquey-ui-datepicker.js
@@ -103,7 +103,7 @@ $(document).ready(function() {
                         }
                         var field = document.getElementById(k);
                         //field.setAttribute('pattern', '(0[1-9]|1[0-9]|2[0-9]|3[01])-(0[1-9]|1[012])-[0-9]{4}');
-                        field.setAttribute('pattern', '([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))');
+                        field.setAttribute('pattern', '[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])');
                     }
                 });
             }
