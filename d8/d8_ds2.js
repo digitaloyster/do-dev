b42393512757f128fc13a690bae5fa9efce3d61d
Simon Oliver <49720361+simondigitaloyster@users.noreply.github.com>
2019-11-07 16:40:52 +0000

    Update d8_ds2.js

--- d8/d8_ds2.js
+++ d8/d8_ds2.js
@@ -1,15 +1,4 @@
 window.onload = function(){
-   /* $('body').after('<style id="postcode-switch">#container_postcode .error-message {display:none !important;}</style>');
-    $('#address_search').change(function() {
-      if ($('#address_search').val() != '' && $('#postcode').val() == '') {
-        $('#container_address_search .error-message').remove();
-        $('#postcode-switch').text('#container_postcode .error-message {display:block !important;}');
-      } 
-      else {
-        $('#postcode-switch').text('#container_postcode .error-message {display:none !important;}');
-      }
-    }); 
-    $( '#address_search' ).after( '<div id="address"></div> ');*/
     var txt = document.getElementById('postcode');
     new data8.predictiveaddressui(txt, {
       ajaxKey: 'TJYU-SWL8-DN53-XQF4',
@@ -22,12 +11,6 @@ window.onload = function(){
       ],
       allowedCountries: ["GB"],
       selectAddress: function(pa, results) {
-        /*$('#label_address_search').hide();
-        $('#address_search').val( pa.Address.Lines[5] ).hide();
-        let this_add = pa.Address.Lines.filter( function( x ){ if( x != ''){ return x; } })
-        $("#address").html( this_add.join('<br/>') );
-        $("#address").after("<button id='address-edit'>&#9998;</button>");
-        $('.error-message').remove(); */
       },
       showResults: function( pa, results ){  
          if (!results.Count) {
